6679cce86842448fae9e3782bc69f26c66ba2ae4
Adeleye Temiloluwa <temiloluwagithub@gmail.com>
2025-04-27 00:47:06 +0100

    refactor: Clean up question repository code for improved readability and consistency

--- src/api/questions/question.repository.ts
+++ src/api/questions/question.repository.ts
@@ -2,25 +2,65 @@ import type { Prisma } from "@prisma/client";
 import { prisma } from "../../db/index.js";
 
 export const createQuestion = async (dto: Prisma.QuestionCreateInput) => {
-    return await prisma.question.create({data: dto})
-}
+  return await prisma.question.create({ data: dto });
+};
 
-export const getQuestionById = async (id: {id: string}) => {
-    return await prisma.question.findUnique({
-        where: { id: id.id },
-        include: {
-            answers: {
-                select: {
-                    id: true,
-                    answerText: true,
-                    answerJson: true,
-                    answerNumber: true,
-                    questionId: true,
-                    createdAt: true,
-
-                },
-            }
+export const getQuestionById = async (id: { id: string }) => {
+  return await prisma.question.findUnique({
+    where: { id: id.id },
+    include: {
+      answers: {
+        select: {
+          id: true,
+          answerText: true,
+          answerJson: true,
+          answerNumber: true,
+          questionId: true,
+          createdAt: true,
         },
-    });
-}
+      },
+    },
+  });
+};
 
+export const getQuestionsBySurveyId = async ({
+  surveyId,
+}: {
+  surveyId: string;
+}) => {
+  return await prisma.question.findMany({
+    where: { surveyId },
+    // include: {
+    //     answers: {
+    //         select: {
+    //             id: true,
+    //             answerText: true,
+    //             answerJson: true,
+    //             answerNumber: true,
+    //             questionId: true,
+    //             createdAt: true,
+
+    //         },
+    //     }
+    // },
+  });
+};
+
+export const updateQuestion = async ({
+  questionId,
+  dto,
+}: {
+  questionId: string;
+  dto: Prisma.QuestionUpdateInput;
+}) => {
+  return await prisma.question.update({
+    where: { id: questionId },
+    data: dto,
+  });
+};
+
+export const deleteQuestion = async (id: { id: string }) => {
+  return await prisma.question.delete({
+    where: { id: id.id },
+  });
+}
