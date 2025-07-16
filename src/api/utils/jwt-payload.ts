262f27a6c330970ae72595c604463bdc0579fe10
Adeleye Temiloluwa <temiloluwagithub@gmail.com>
2025-07-16 04:32:26 +0100

    fixed jwt payload

--- src/api/utils/jwt-payload.ts
+++ src/api/utils/jwt-payload.ts
@@ -1,24 +1,11 @@
 import type { Context } from "hono";
 import type { JWTPayload } from "../middleware/index.js";
-import { verify } from "hono/jwt";
 
-export const getJWTPayload = async (c: Context): Promise<JWTPayload> => {
-  let payload = c.get("jwt_payload") as JWTPayload;
-  if (!payload) {
-    const authHeader = c.req.header("Authorization");
-    console.log({authHeader})
-    if (authHeader && authHeader.startsWith("Bearer ")) {
-      const token = authHeader.substring(7);
-      try {
-        payload = (await verify(token, process.env.JWT_SECRET!)) as JWTPayload;
-      } catch (error) {
-        throw new Error("Invalid JWT token");
-      }
+export const getJWTPayload = (c: Context): JWTPayload => {
+    const payload = c.get("jwt_payload") as JWTPayload;
+    if (!payload) {
+        throw new Error("JWT payload not found");
     }
-  }console.log("No payload!")
-  if (!payload) {
-    throw new Error("JWT payload not found");
-  }
-
-  return payload;
-};
+    return payload;
+    
+}
